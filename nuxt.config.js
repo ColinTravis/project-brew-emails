7bf0396baee582033e036538d7ccfb0ef353e7b6
ColinTravis <colint@cybertravis.us>
2024-02-08 15:59:16 -0600

    Switching over to mjml

--- nuxt.config.js
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@vue-email/nuxt"],
-  vueEmail: {
-    baseUrl: "https://vue-email-demo.vercel.app/",
-    autoImport: true,
-  },
-});
